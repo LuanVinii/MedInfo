a49ec962ffb590b04860f2abd6485ea0eb88a49e
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-09-22 18:03:55 -0300

    fix: adicionar componente TopBar com menu, logo e perfil

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -33,6 +33,7 @@ class HomeView extends StatelessWidget {
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
+                        TopBar(),
                         SizedBox(height: 20),
                       ],
                     ),
@@ -56,6 +57,31 @@ class Background extends StatelessWidget {
       child: Image.asset(
         'assets/images/background.png',
         fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+          ),
+          Image.asset('assets/images/LogoMedInfo.png', height: 50),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person, color: Colors.black, size: 30),
+          ),
+        ],
       ),
     );
   }
