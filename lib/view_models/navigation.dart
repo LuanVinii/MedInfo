34b7978e69cee7873682b10dde3eb705b2deb49a
Denilson Santos <144867249+denilsonsantos-dev@users.noreply.github.com>
2025-11-29 00:54:47 -0300

    fix: atualização no recurso de navegação do app
    NavigationViewModel agora contém o método changeViewReplacing, que permite navegar para uma outra tela sem possibilidade de retorno para a anterior.

--- lib/view_models/navigation.dart
+++ lib/view_models/navigation.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:medinfo/views/ajustes.dart';
 import 'package:medinfo/views/bookmarks.dart';
 import 'package:medinfo/views/categorias.dart';
-import 'package:medinfo/widgets/globais.dart';
 
 import '../views/home.dart';
 
@@ -25,6 +24,17 @@ class NavigationViewModel extends StateNotifier<NavigationViewModelState> {
   void changeView(Widget targetView, BuildContext context, {int? newIndex}) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => targetView),
+      );
+    });
+    if (newIndex != null) {
+      state = NavigationViewModelState(currentIndex: newIndex);
+    }
+  }
+
+  void changeViewReplacing(Widget targetView, BuildContext context, {int? newIndex}) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => targetView),
       );
     });
